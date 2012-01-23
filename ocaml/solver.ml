97af0f68cbef2d296e4f06bdeebe20bac0cca40d
Robert Martin <rdmartin3@gmail.com>
2012-01-23 11:18:07 -0500

    Broken additions to try to enumerate combinations in the form n-choose-k

--- ocaml/solver.ml
+++ ocaml/solver.ml
@@ -12,14 +12,6 @@
 
 open Bigarray
 
-(* TODO: Delete this testing function *)
-let dummy_outcomes cnt players outcomes =
-    for i = 1 to (1 lsl cnt) - 1 do
-        let rec choice x =
-            if x mod 2 = 1 then 0 else 1 + choice (x lsr 1)
-        in
-        outcomes.(i) <- choice i
-    done
 
 let generate_initial_blocks cnt =
     (* Generate a list of the # of mouths in each scenario *)
@@ -37,25 +29,34 @@ let generate_initial_blocks cnt =
     let blocks = List.map gen_block counts in
     Array.of_list blocks
 
-let rec do_calc_blocks cnt players outcomes blocks mouths ix =
-    (* Calculate the block for the ix permutation of mouths *)
-    (* Recursively calculate the next permutation *)
-    if (ix = mouths) then
-        (* TODO: Replace with () once dummy_outcomes is not necessary *)
-        dummy_outcomes cnt players outcomes
-    else (
-        (* TODO: Calculate this block here! *)
-        do_calc_blocks cnt players outcomes blocks mouths (ix + 1)
-    )
+let analyze_block cnt players outcomes blocks mouths p =
+    (* TODO: Use this block to generate new block rows *)
+    print_endline("Analyze blocks for iteration" ^ (string_of_int p));
+    ()
 
-let calc_blocks cnt players outcomes blocks mouths =
-    do_calc_blocks cnt players outcomes blocks mouths 0
+let update_outcomes cnt players outcomes blocks mouths p =
+    (* XXX: Dummy update_outcomes *)
+    print_endline("Update outcome #" ^ (string_of_int p));
+    let rec dummy_choice x =
+        if x mod 2 = 1 then 0 else 1 + dummy_choice (x lsr 1)
+    in
+    outcomes.(p) <- dummy_choice p
+
+let rec calc_blocks cnt players outcomes blocks mouths p =
+    (* Calculate the block for the ix combination of mouths *)
+    (* Recursively calculate the next combination *)
+    try
+        analyze_block cnt players outcomes blocks mouths p;
+        update_outcomes cnt players outcomes blocks mouths p;
+        calc_blocks cnt players outcomes blocks mouths (Combin.next_combin cnt p)
+    with Combin.LastCombinError -> ()
 
 let solve_outcomes cnt players =
     let outcomes = Array.make (1 lsl cnt) (-1) in
     let blocks = generate_initial_blocks cnt in
     for i = 2 to cnt do
-        calc_blocks cnt players outcomes blocks i;
+        print_endline("Calc blocks of size " ^ (string_of_int i));
+        calc_blocks cnt players outcomes blocks i (Combin.first_combin cnt i)
     done;
     outcomes
 
--- ocaml/utils.ml
+++ ocaml/utils.ml
@@ -24,4 +24,25 @@ let print_matrix mat =
     in
     Array.iter print_row mat
 
+let get_bit n bitnum =
+    if (1 lsl bitnum) land n > 0 then 1 else 0
+
+let set_bit n bitnum value =
+    let mask = 1 lsl bitnum in
+    if value = 1 then
+        n lor mask
+    else
+        n land lnot mask
+
+let string_of_bin num =
+    if num = 0 then
+        "0"
+    else
+        let rec do_print_bin n =
+            if n = 0 then
+                ""
+            else
+                (do_print_bin (n lsr 1)) ^ (string_of_int (n land 1))
+        in
+        do_print_bin num
 
