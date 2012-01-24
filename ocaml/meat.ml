b743467d9da059e5d03fb3d213d11a4fab8f2fe8
Robert Martin <rdmartin3@gmail.com>
2012-01-24 00:54:38 -0500

    Add more groundwork for calculating next column

--- ocaml/meat.ml
+++ ocaml/meat.ml
@@ -26,26 +26,36 @@ let update_outcomes players blocks outcomes scenario =
     );
     outcomes.(scenario) <- dummy_choice scenario
 
-let rec analyze_block_columns players blocks scenario ancestors =
+let calc_column old_scenario old_block old_victim new_block new_victim days =
+    (* let new_scenario = old_scenario land (1 lsl victim) in *)
+    (* XXX: Calc column from old column *)
+    print_endline("\tCalc: If " ^ (string_of_int new_victim) ^ " dies...")
+
+let rec analyze_block_columns players blocks outcomes scenario ancestors =
     let cnt = Array.length players in
+    let mouths = Utils.count_bits scenario in
     match ancestors with
     | hd :: tl ->
-            print_endline("Analyze column for ancestor " ^
-            (string_of_int hd) ^
-            " of scenario " ^
-            (Utils.string_of_comb cnt scenario));
-            analyze_block_columns players blocks scenario tl
+        (
+            let days = Utils.divide_round_up (fst players.(hd)) mouths in
+            let old_victim = outcomes.(scenario) in
+            let old_block = blocks.(scenario) in
+            let new_scenario = scenario land (1 lsl hd) in
+            let new_block = blocks.(new_scenario) in
+                print_endline("Analyze column for ancestor " ^
+                (string_of_int hd) ^
+                " of scenario " ^
+                (Utils.string_of_comb cnt scenario));
+            calc_column scenario old_block old_victim new_block hd days;
+            analyze_block_columns players blocks outcomes scenario tl
+        )
     | [] -> ()
 
-
-
-let analyze_block players blocks scenario =
-    (* XXX: Use this block to generate new block rows *)
+let analyze_block players blocks outcomes scenario =
     let cnt = Array.length players in
     print_endline("Analyzing block " ^ (Utils.string_of_comb cnt scenario) ^ ":");
     let ancestors = Combin.ancestors_of_scenario cnt scenario in
-    analyze_block_columns players blocks scenario ancestors
-
+    analyze_block_columns players blocks outcomes scenario ancestors
 
 let rec calc_blocks players blocks outcomes mouths =
     let cnt = Array.length players in
@@ -58,7 +68,7 @@ let rec calc_blocks players blocks outcomes mouths =
         print_endline("Analyzing blocks for " ^ (string_of_int cnt) ^ "-choose-" ^ (string_of_int mouths));
         (* 2. Analyze each of the blocks *)
         let combins = Combin.get_all_combinations cnt mouths in
-        let _ = List.map (analyze_block players blocks) combins in
+        let _ = List.map (analyze_block players blocks outcomes) combins in
         ()
     );
     (* 3. Update the outcomes for each block *)
