668cb892ecd3e821b31bcdc6f73541c3fffbac58
Robert Martin <rdmartin3@gmail.com>
2012-01-25 00:44:57 -0500

    Rewrite dummy voting to begin to resemble actual naive algorithm

--- ocaml/vote.ml
+++ ocaml/vote.ml
@@ -1,5 +1,16 @@
 
-(* Naive Vote:
+let rec collect_players scenario players =
+    match players with
+    | [] -> []
+    | hd :: tl ->
+        if scenario land 1 = 1 then
+            (hd :: collect_players (scenario lsr 1) tl)
+        else
+            (collect_players (scenario lsr 1) tl)
+
+
+(*
+ * Naive Vote:
  * Each player votes for eating the person who gives them the longest life.
  * The largest plurality wins. If there is a tie, the smaller person is the
  * victim. If they are the same size, the alphabetically earlier person is
@@ -7,10 +18,20 @@
  *)
 let naive_vote players block scenario =
     (* XXX: Dummy update_outcomes *)
-    let rec dummy_choice n =
-        let next_n = n lsr 1 in
-        if next_n = 0 then 0 else 1 + dummy_choice next_n
-    in
-    dummy_choice scenario
+    let players = Array.mapi (fun i p -> (i, p)) players in
+    let players = collect_players scenario (Array.to_list players) in
+    let (i, p) = List.hd (List.rev players) in
+    i
 
 
+
+
+
+
+(*
+ * Candorcet Vote:
+ * See README for details.
+ *)
+let candorcet_vote players block scenario =
+    (* TODO: Implement this *)
+    failwith "Candorcet voting not yet implemented."
